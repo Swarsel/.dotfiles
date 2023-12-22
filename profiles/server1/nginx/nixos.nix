9591f783f354175e0172b4da1eb36011899c0f1c
Swarsel <leon.schwarzaeugl@gmail.com>
2023-12-22 21:42:40 +0100

    nginx

--- profiles/server1/nginx/nixos.nix
+++ profiles/server1/nginx/nixos.nix
@@ -94,20 +94,20 @@
             "/" = {
               proxyPass = "https://192.168.2.13";
               extraConfig = ''
-                proxy_read_timeout 36000s;
-                proxy_http_version 1.1;
-                proxy_buffering off;
+                # proxy_read_timeout 36000s;
+                # proxy_http_version 1.1;
+                # proxy_buffering off;
                 client_max_body_size 0;
-                proxy_redirect off;
-                proxy_set_header Connection "";
-                proxy_set_header Host $host;
-                proxy_set_header X-Real-IP $remote_addr;
-                proxy_set_header X_Forwarded_For $proxy_add_x_forwarded_for;
-                proxy_set_header X-Forwarded-Host $host;
-                proxy_set_header X-Forwarded-Proto $scheme;
-                proxy_hide_header X-Powered-By;
-                proxy_hide_header X-Frame-Options;
-                proxy_pass_header Authorization;
+                # proxy_redirect off;
+                # proxy_set_header Connection "";
+                # proxy_set_header Host $host;
+                # proxy_set_header X-Real-IP $remote_addr;
+                # proxy_set_header X_Forwarded_For $proxy_add_x_forwarded_for;
+                # proxy_set_header X-Forwarded-Host $host;
+                # proxy_set_header X-Forwarded-Proto $scheme;
+                # proxy_hide_header X-Powered-By;
+                # proxy_hide_header X-Frame-Options;
+                # proxy_pass_header Authorization;
               '';
             };
           };
