d5aeaf64cf3615fc40f1262376022f991c3d2895
Swarsel <leon.schwarzaeugl@gmail.com>
2023-12-23 00:50:03 +0100

    try

--- profiles/server1/calibre/nixos.nix
+++ profiles/server1/calibre/nixos.nix
@@ -52,12 +52,12 @@
   };
 
   fileSystems."/media/books" = {
-    device = "//192.168.1.3/Eternor";
+    device = "//192.168.1.3/Eternor/Books";
     fsType = "cifs";
     options = let
       # this line prevents hanging on network split
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=${config.sops.templates."smb.cred".path},uid=1000,gid=100"];
+    in ["${automount_opts},credentials=${config.sops.templates."smb.cred".path}"];
   };
 
 #   services.calibre-server = {
