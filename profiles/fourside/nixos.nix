fb9feab3e1a513174dfcd35c9e85c2dc31c60bb7
Swarsel <leon.schwarzaeugl@gmail.com>
2024-02-19 20:53:28 +0100

    add minecraft to fourside

--- profiles/fourside/nixos.nix
+++ profiles/fourside/nixos.nix
@@ -101,6 +101,25 @@
   };
 
   environment.systemPackages = with pkgs; [
+    temurin-bin-17
+
+    (prismlauncher.override {
+      glfw = (let
+        mcWaylandPatchRepo = fetchFromGitHub {
+          owner = "Admicos";
+          repo = "minecraft-wayland";
+          rev = "370ce5b95e3ae9bc4618fb45113bc641fbb13867";
+          sha256 =
+            "sha256-RPRg6Gd7N8yyb305V607NTC1kUzvyKiWsh6QlfHW+JE=";
+        };
+        mcWaylandPatches = map (name: "${mcWaylandPatchRepo}/${name}")
+          (lib.naturalSort (builtins.attrNames (lib.filterAttrs
+          (name: type:
+            type == "regular" && lib.hasSuffix ".patch" name)
+            (builtins.readDir mcWaylandPatchRepo))));
+      in glfw-wayland.overrideAttrs (previousAttrs: {
+        patches = previousAttrs.patches ++ mcWaylandPatches;
+      }));})
   ];
 
   system.stateVersion = "23.05";
