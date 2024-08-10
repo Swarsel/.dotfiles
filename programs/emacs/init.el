40ccc0a9e94bfe39a7363791d32ea1c0f9ee0b63
Swarsel <leon.schwarzaeugl@gmail.com>
2024-08-10 19:22:58 +0200

    fix: broken links in index.html, broken link marks

--- programs/emacs/init.el
+++ programs/emacs/init.el
@@ -950,6 +950,8 @@ create a new one."
 
 (push '("conf-unix" . conf-unix) org-src-lang-modes)
 
+(setq org-export-with-broken-links mark)
+
 (require 'org-tempo)
 (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
 (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
