332dee17b8f82d7310be055340706e0264343747
Swarsel <leon.schwarzaeugl@gmail.com>
2024-10-18 21:34:09 +0200

    feat: add packer env wars for work

---
+++ profiles/server/common/monitoring.nix
@@ -0,0 +1,43 @@
+{ pkgs, lib, config, ... }:
+{
+  config = lib.mkIf config.swarselsystems.server.monitoring {
+
+    sops.secrets = {
+      grafanaadminpass = {
+        owner = "grafana";
+      }
+        }
+        users.users.grafana = {
+        extraGroups = [ "users" ];
+    };
+
+    services.grafana = {
+      enable = true;
+      dataDir = "/Vault/data/grafana";
+      admin_password = "$__file{/run/secrets/grafanaadminpass}";
+      settings = {
+        http_port = 3000;
+        http_addr = "127.0.0.1";
+      };
+    };
+
+    services.nginx = {
+      virtualHosts = {
+        "status.swarsel.win" = {
+          enableACME = true;
+          forceSSL = true;
+          acmeRoot = null;
+          locations = {
+            "/" = {
+              proxyPass = "http://localhost:3000/";
+              extraConfig = ''
+                client_max_body_size 0;
+              '';
+            };
+          };
+        };
+      };
+    };
+  };
+
+}
