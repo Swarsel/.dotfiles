ca1eaf7c17db6971339a5eb58feb8fbe36f17531
Swarsel <leon.schwarzaeugl@gmail.com>
2024-10-19 00:35:37 +0200

    wip: grafana

--- profiles/server/common/monitoring.nix
+++ profiles/server/common/monitoring.nix
@@ -25,19 +25,21 @@
               name = "prometheus";
               type = "prometheus";
               url = "https://status.swarsel.win/prometheus";
-              editable = true;
+              editable = false;
               access = "proxy";
               basicAuth = true;
               basicAuthUser = "admin";
-              basicAuthPassword = "$__file{/run/secrets/prometheusadminpass}";
               jsonData = {
                 httpMethod = "POST";
                 manageAlerts = true;
                 prometheusType = "Prometheus";
-                prometheusVersion = ">2.50.x";
+                prometheusVersion = "> 2.50.x";
                 cacheLevel = "High";
                 disableRecordingRules = false;
                 incrementalQueryOverlapWindow = "10m";
+              };
+              secureJsonData = {
+                basicAuthPassword = "$__file{/run/secrets/prometheusadminpass}";
               };
             }
           ];
@@ -60,8 +62,31 @@
       webExternalUrl = "https://status.swarsel.win/prometheus";
       port = 9090;
       listenAddress = "127.0.0.1";
+      globalConfig = {
+        scrape_interval = "10s";
+      };
       webConfigFile = ../../../programs/server/prometheus/web.config;
+      scrapeConfigs = [
+        {
+          job_name = "node";
+          static_configs = [{
+            targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
+          }];
+        }
+        {
+          job_name = "zfs";
+          static_configs = [{
+            targets = [ "localhost:${toString config.services.prometheus.exporters.zfs.port}" ];
+          }];
+        }
+      ];
       exporters = {
+        node = {
+          enable = true;
+          port = 9000;
+          enabledCollectors = [ "systemd" ];
+          extraFlags = [ "--collector.ethtool" "--collector.softirqs" "--collector.tcpstat" "--collector.wifi" ];
+        };
         zfs = {
           enable = true;
           port = 9134;
@@ -71,30 +96,31 @@
         };
       };
     };
+  };
 
-    services.nginx = {
-      virtualHosts = {
-        "status.swarsel.win" = {
-          enableACME = true;
-          forceSSL = true;
-          acmeRoot = null;
-          locations = {
-            "/" = {
-              proxyPass = "http://localhost:3000";
-              extraConfig = ''
-                client_max_body_size 0;
-              '';
-            };
-            "/prometheus" = {
-              proxyPass = "http://localhost:9090";
-              extraConfig = ''
-                client_max_body_size 0;
-              '';
-            };
+  services.nginx = {
+    virtualHosts = {
+      "status.swarsel.win" = {
+        enableACME = true;
+        forceSSL = true;
+        acmeRoot = null;
+        locations = {
+          "/" = {
+            proxyPass = "http://localhost:3000";
+            extraConfig = ''
+              client_max_body_size 0;
+            '';
+          };
+          "/prometheus" = {
+            proxyPass = "http://localhost:9090";
+            extraConfig = ''
+              client_max_body_size 0;
+            '';
           };
         };
       };
     };
   };
+};
 
 }
