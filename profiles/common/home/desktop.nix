29f2d5977e293d770199ee78c3aaa4e308e0c320
Swarsel <leon.schwarzaeugl@gmail.com>
2024-12-03 18:50:23 +0100

    fix: set default apps for mime types by hand

--- profiles/common/home/desktop.nix
+++ profiles/common/home/desktop.nix
@@ -36,4 +36,49 @@ _:
     };
 
   };
+
+  xdg.mimeApps = {
+
+    enable = true;
+    defaultApplications = {
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/chrome" = [ "firefox.desktop" ];
+      "text/plain" = [ "emacsclient.desktop" ];
+      "text/csv" = [ "emacsclient.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+      "application/x-extension-htm" = [ "firefox.desktop" ];
+      "application/x-extension-html" = [ "firefox.desktop" ];
+      "application/x-extension-shtml" = [ "firefox.desktop" ];
+      "application/xhtml+xml" = [ "firefox.desktop" ];
+      "application/x-extension-xhtml" = [ "firefox.desktop" ];
+      "application/x-extension-xht" = [ "firefox.desktop" ];
+      "image/png" = [ "imv.desktop" ];
+      "image/jpeg" = [ "imv.desktop" ];
+      "image/gif" = [ "imv.desktop" ];
+      "image/svg" = [ "imv.desktop" ];
+      "image/webp" = [ "firefox.desktop" ];
+      "image/vnd.adobe.photoshop" = [ "gimp.desktop" ];
+      "image/vnd.dxf" = [ "org.inkscape.Inkscape.desktop" ];
+      "audio/flac" = [ "mpv.desktop" ];
+      "audio/mp3" = [ "mpv.desktop" ];
+      "audio/ogg" = [ "mpv.desktop" ];
+      "audio/wav" = [ "mpv.desktop" ];
+      "video/mp4" = [ "umpv.desktop" ];
+      "video/mkv" = [ "umpv.desktop" ];
+      "video/flv" = [ "umpv.desktop" ];
+      "video/3gp" = [ "umpv.desktop" ];
+      "application/pdf" = [ "org.gnome.Evince.desktop" ];
+      "application/metalink+xml" = [ "emacsclient.desktop" ];
+      "application/sql" = [ "emacsclient.desktop" ];
+      "application/vnd.ms-powerpoint" = [ "impress.desktop" ];
+      "application/msword" = [ "writer.desktop" ];
+      "application/vnd.ms-excel" = [ "calc.desktop" ];
+    };
+    associations = {
+      added = {
+        "application/x-zerosize" = [ "emacsclient.desktop" ];
+      };
+    };
+  };
 }
