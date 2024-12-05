35d5ec57b85ec8600ddb8aa70bc162abe347a6ff
Swarsel <leon.schwarzaeugl@gmail.com>
2024-12-05 04:14:05 +0100

    chore: updaate .elfeed path

--- programs/emacs/server.el
+++ programs/emacs/server.el
@@ -43,7 +43,7 @@
   :ensure t
   :config
   (elfeed-org)
-  (setq rmh-elfeed-org-files (list "/Vault/data/syncthing/.elfeed/elfeed.org"))
+  (setq rmh-elfeed-org-files (list "/home/swarsel/.elfeed/elfeed.org"))
   )
 
 ;; Laod elfeed-goodies
