5827b53c6cc4a34850c83ef22ceb63b72d4d09e8
Swarsel <leon.schwarzaeugl@gmail.com>
2024-12-15 20:27:56 +0100

    feat: ssh agentForward for root (and other users)

--- profiles/server/nixos/ssh.nix
+++ profiles/server/nixos/ssh.nix
@@ -11,5 +11,8 @@
     (self + /secrets/keys/ssh/nbl-imba-2.pub)
     (self + /secrets/keys/ssh/magicant.pub)
   ];
+  security.sudo.extraConfig = ''
+    Defaults    env_keep+=SSH_AUTH_SOCK
+  '';
 
 }
