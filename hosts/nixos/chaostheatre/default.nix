6cba256e0b3f4beb0100e120b365196f1fe9414d
Swarsel <leon.schwarzaeugl@gmail.com>
2024-12-18 12:52:16 +0100

    feat: chaostheatre demo system

---
+++ hosts/nixos/chaostheatre/default.nix
@@ -0,0 +1,51 @@
+{ self, inputs, outputs, config, pkgs, lib, ... }:
+let
+  profilesPath = "${self}/profiles";
+in
+{
+
+  imports = outputs.nixModules ++ [
+
+    ./hardware-configuration.nix
+
+    "${profilesPath}/optional/nixos/autologin.nix"
+
+    inputs.home-manager.nixosModules.home-manager
+    {
+      home-manager.users.swarsel.imports = outputs.mixedModules ++ (builtins.attrValues outputs.homeManagerModules);
+    }
+  ] ++ (builtins.attrValues outputs.nixosModules);
+
+
+  nixpkgs = {
+    overlays = [ outputs.overlays.default ];
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+
+  boot = {
+    loader.systemd-boot.enable = lib.mkForce true;
+    loader.efi.canTouchEfiVariables = true;
+    kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+  };
+
+  networking = {
+    hostName = "chaostheatre";
+    firewall.enable = true;
+  };
+
+
+  swarselsystems = {
+    wallpaper = self + /wallpaper/lenovowp.png;
+    initialSetup = true;
+    isPublic = true;
+  };
+
+  home-manager.users.swarsel.swarselsystems = {
+    isNixos = true;
+    isPublic = true;
+    flakePath = "/home/swarsel/.dotfiles";
+  };
+}
