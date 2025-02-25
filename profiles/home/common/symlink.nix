6279713ab526b75f0493ed9cfb05566a289ffaea
Leon Schwarzäugl <leon.schwarzaeugl@imba.oeaw.ac.at>
2025-02-25 22:53:12 +0100

    style: summarize some sections

--- profiles/home/common/symlink.nix
+++ profiles/home/common/symlink.nix
@@ -18,14 +18,11 @@
       source = self + /programs/git/.gitmessage;
       target = ".gitmessage";
     };
-    "swayidle/config" = {
-      source = self + /programs/swayidle/config;
-      target = ".config/swayidle/config";
-    };
   };
 
   xdg.configFile = {
     "tridactyl/tridactylrc".source = self + /programs/firefox/tridactyl/tridactylrc;
     "tridactyl/themes/base16-codeschool.css".source = self + /programs/firefox/tridactyl/themes/base16-codeschool.css;
+    "swayidle/config".source = self + /programs/swayidle/config;
   };
 }
