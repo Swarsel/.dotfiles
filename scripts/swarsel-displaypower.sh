27b06772c2eb228a8e4e0c4fbe771b47edd68924
Leon Schwarzäugl <leon.schwarzaeugl@imba.oeaw.ac.at>
2025-03-25 02:38:52 +0100

    fix: full swaylock config again

--- scripts/swarsel-displaypower.sh
+++ scripts/swarsel-displaypower.sh
@@ -1,2 +1,2 @@
-swaymsg output eDP-1 power on > /dev/null 2>&1 || true
-swaymsg output eDP-2 power on > /dev/null 2>&1 || true
+swaymsg "output * power on" > /dev/null 2>&1 || true
+swaymsg "output * dpms on" > /dev/null 2>&1 || true
