1e438b8eff0954d8aeaced9c4d917835378f336c
Leon Schwarzäugl <leon.schwarzaeugl@imba.oeaw.ac.at>
2025-03-25 02:35:55 +0100

    chore: only set defaultStrategy for fw-fanctrl

--- hosts/nixos/nbl-imba-2/default.nix
+++ hosts/nixos/nbl-imba-2/default.nix
@@ -60,23 +60,9 @@ in
 
   programs.fw-fanctrl = {
     enable = true;
-    # config = {
-    #   defaultStrategy = "lazy";
-    #   strategies = {
-    #     "lazy" = {
-    #       fanSpeedUpdateFrequency = 5;
-    #       movingAverageInterval = 30;
-    #       speedCurve = [
-    #         { temp = 0; speed = 15; }
-    #         { temp = 50; speed = 15; }
-    #         { temp = 65; speed = 25; }
-    #         { temp = 70; speed = 35; }
-    #         { temp = 75; speed = 50; }
-    #         { temp = 85; speed = 100; }
-    #       ];
-    #     };
-    #   };
-    # };
+    config = {
+      defaultStrategy = "lazy";
+    };
   };
 
   networking = {
