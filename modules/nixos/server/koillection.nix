6fb45a020093fe89dd402b052071aa30f4294799
Leon Schwarzäugl <leon.schwarzaeugl@imba.oeaw.ac.at>
2025-07-20 16:01:19 +0200

    fix: koillection not serving big collections

--- modules/nixos/server/koillection.nix
+++ modules/nixos/server/koillection.nix
@@ -119,6 +119,11 @@ in
           locations = {
             "/" = {
               proxyPass = "http://${serviceName}";
+              extraConfig = ''
+                proxy_buffer_size          128k;
+                proxy_buffers              4 256k;
+                proxy_busy_buffers_size    256k;
+              '';
             };
           };
         };
