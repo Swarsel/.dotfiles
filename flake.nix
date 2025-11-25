fd6df781cf8857fda6e473bc6919998071e4cfd2
Leon Schwarzäugl <leon.schwarzaeugl@imba.oeaw.ac.at>
2025-11-25 19:40:15 +0100

    chore: cleanup flake inputs

--- flake.nix
+++ flake.nix
@@ -11,100 +11,57 @@
   };
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    smallpkgs.url = "github:nixos/nixpkgs/08fcb0dcb59df0344652b38ea6326a2d8271baff?narHash=sha256-HXIQzULIG/MEUW2Q/Ss47oE3QrjxvpUX7gUl4Xp6lnc%3D&shallow=1";
     nixpkgs-dev.url = "github:Swarsel/nixpkgs/main";
     nixpkgs-kernel.url = "github:NixOS/nixpkgs/063f43f2dbdef86376cc29ad646c45c46e93234c?narHash=sha256-6m1Y3/4pVw1RWTsrkAK2VMYSzG4MMIj7sqUy7o8th1o%3D"; #specifically pinned for kernel version
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-stable24_05.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-stable24_11.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-stable25_05.url = "github:NixOS/nixpkgs/nixos-25.05";
-    systems.url = "github:nix-systems/default";
-    swarsel-modules.url = "github:Swarsel/swarsel-modules/main";
-    swarsel-nix.url = "github:Swarsel/swarsel-nix/main";
+
     home-manager = {
       # url = "github:nix-community/home-manager";
       url = "github:Swarsel/home-manager/main";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    swarsel.url = "github:Swarsel/.dotfiles";
-    emacs-overlay = {
-      # url = "github:nix-community/emacs-overlay";
-      url = "github:nix-community/emacs-overlay/aba8daa237dc07a3bb28a61c252a718e8eb38057?narHash=sha256-4OXXccXsY1sBXTXjYIthdjXLAotozSh4F8StGRuLyMQ%3D";
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # emacs-overlay.url = "github:nix-community/emacs-overlay";
+    emacs-overlay.url = "github:nix-community/emacs-overlay/aba8daa237dc07a3bb28a61c252a718e8eb38057?narHash=sha256-4OXXccXsY1sBXTXjYIthdjXLAotozSh4F8StGRuLyMQ%3D";
+    swarsel-nix.url = "github:Swarsel/swarsel-nix/main";
+    systems.url = "github:nix-systems/default";
     nur.url = "github:nix-community/NUR";
     nixgl.url = "github:guibou/nixGL";
     stylix.url = "github:danth/stylix";
     sops-nix.url = "github:Mic92/sops-nix";
     lanzaboote.url = "github:nix-community/lanzaboote";
-    nix-on-droid = {
-      url = "github:nix-community/nix-on-droid/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixos-generators = {
-      url = "github:nix-community/nixos-generators";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixos-hardware = {
-      url = "github:NixOS/nixos-hardware/master";
-    };
-    nswitch-rcm-nix = {
-      url = "github:Swarsel/nswitch-rcm-nix";
-    };
-    nix-index-database = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nix-on-droid.url = "github:nix-community/nix-on-droid/release-24.05";
+    nixos-generators.url = "github:nix-community/nixos-generators";
+    nixos-images.url = "github:Swarsel/nixos-images/main";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nswitch-rcm-nix.url = "github:Swarsel/nswitch-rcm-nix";
+    disko.url = "github:nix-community/disko";
     impermanence.url = "github:nix-community/impermanence";
-    zjstatus = {
-      url = "github:dj95/zjstatus";
-    };
-    # has been upstreamed
-    # fw-fanctrl = {
-    #   # url = "github:TamtamHero/fw-fanctrl/packaging/nix";
-    #   url = "github:Swarsel/fw-fanctrl/packaging/nix";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-    nix-darwin = {
-      url = "github:lnl7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    pre-commit-hooks = {
-      url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    vbc-nix = {
-      url = "git+ssh://git@github.com/vbc-it/vbc-nix.git?ref=main";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    zjstatus.url = "github:dj95/zjstatus";
+    nix-darwin.url = "github:lnl7/nix-darwin";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+    vbc-nix.url = "git+ssh://git@github.com/vbc-it/vbc-nix.git?ref=main";
     nix-topology.url = "github:oddlama/nix-topology";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    devshell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    spicetify-nix = {
-      url = "github:Gerg-l/spicetify-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    niri-flake = {
-      url = "github:sodiboo/niri-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixos-extra-modules = {
-      url = "github:oddlama/nixos-extra-modules";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    microvm = {
-      url = "github:astro/microvm.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    devshell.url = "github:numtide/devshell";
+    spicetify-nix.url = "github:Gerg-l/spicetify-nix";
+    niri-flake.url = "github:sodiboo/niri-flake";
+    nixos-extra-modules.url = "github:oddlama/nixos-extra-modules/main";
+    microvm.url = "github:astro/microvm.nix";
     treefmt-nix.url = "github:numtide/treefmt-nix";
-
+    dns.url = "github:kirelagin/dns.nix";
+    nix-minecraft.url = "github:Infinidoge/nix-minecraft";
+    simple-nixos-mailserver.url = "gitlab:simple-nixos-mailserver/nixos-mailserver/master";
   };
+
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
