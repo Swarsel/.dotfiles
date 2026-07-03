87bf9a659099b9ff587f519be1cf9c13f0979da9
Leon Schwarzäugl <leon.schwarzaeugl@gmail.com>
2026-07-03 17:46:58 +0200

    chore: fix spicectify using deprecated system call

--- modules/client/programs/spicetify.nix
+++ modules/client/programs/spicetify.nix
@@ -14,7 +14,7 @@
         (
           { inputs, pkgs, ... }:
           let
-            spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
+            spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
           in
           {
             swarselsystems.enabledHomeModules = [ "spicetify" ];
