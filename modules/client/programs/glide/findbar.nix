c1d415bccb4a1111e5ed71faa65fbd8017e890e6
Leon Schwarzäugl <leon.schwarzaeugl@gmail.com>
2026-07-11 02:19:20 +0200

    feat[client]: improve glide findbar behaviour

--- modules/client/programs/glide/findbar.nix
+++ modules/client/programs/glide/findbar.nix
@@ -6,6 +6,29 @@
       glide.keymaps.set("normal", "n", () => glide.findbar.next_match());
       glide.keymaps.set("normal", "N", () => glide.findbar.previous_match());
 
+      function enforce_input_mode() {
+        if (glide.ctx.mode !== "normal") {
+          return;
+        }
+        if (glide.commandline.is_active()) {
+          void glide.excmds.execute("mode_change command");
+        } else if (glide.findbar.is_focused()) {
+          void glide.excmds.execute("mode_change insert");
+        }
+      }
+
+      glide.autocmds.create("ModeChanged", "*:normal", enforce_input_mode);
+      setInterval(enforce_input_mode, 100);
+
+      glide.keymaps.set(["insert", "normal"], "<Esc>", () => {
+        if (glide.findbar.is_open()) {
+          void glide.findbar.close();
+        }
+        void glide.excmds.execute("mode_change normal");
+      });
+
+      glide.keymaps.set("command", "<Esc>", () => void glide.commandline.close());
+
       async function tab_or_tabopen(query: string) {
         const url = query.includes("://") ? query : `https://''${query}`;
         const tabs = await browser.tabs.query({ currentWindow: true });
